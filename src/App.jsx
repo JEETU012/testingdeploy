af04208c3a9e252bacd479dfd602ab51ce1f4643
JEETU <jitupanchal012@gmail.com>
2023-11-06 19:43:57 +0530

    #2

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
     } , [])
 
     function fetchfunc(params) {
-      fetch('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b01f7549482f487ba0b83f5bc98d6b0d')
+      fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then( deta => setdata(deta.articles))
+      .then( deta => setdata(deta))
     }
     
     let style = {
